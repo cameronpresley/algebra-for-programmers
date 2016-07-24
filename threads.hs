ad4c5291d1f0910ff8b8feaab667be1e7569b603
Emily Estes <emily.scott.estes@gmail.com>
2016-07-24 19:22:10 -0400

    Fixed some stylistic bits with the Haskell example. Fixed some typos and other minor issues in the slides. Added more slides on Abstract Algebra and started working through Semigroups.

--- threads.hs
+++ threads.hs
@@ -7,7 +7,6 @@ moveOne from to source sink = do
     writeTVar source (from - 1)
     writeTVar sink (to + 1)
 
-
 drainOne source sink = do
     from <- readTVar source
     to <- readTVar sink
@@ -22,28 +21,20 @@ drain name source sink lock = do
             (atomically $ (drainOne source sink)) >> milliSleep 1 >> (drain name source sink lock)
         else return ()
 
-oneDrain lock = do
-    ctr1 <- atomically $ newTVar 5
-    ctr2 <- atomically $ newTVar 10
-    forkIO (do
-        drain "a" ctr1 ctr2 lock)
 
-twoDrains lock = do
-    ctr1 <- atomically $ newTVar 5
-    ctr2 <- atomically $ newTVar 10
-    forkIO (do
-        drain "a" ctr1 ctr2 lock)
-    forkIO (do
-        drain "b" ctr2 ctr1 lock)
+oneDrain lock ctr1 ctr2 = do
+    forkIO $ drain "a" ctr1 ctr2 lock
 
+twoDrains lock ctr1 ctr2 = do
+    forkIO $ drain "a" ctr1 ctr2 lock
+    forkIO $ drain "b" ctr2 ctr1 lock
 
 main = do
     lock <- newMVar ()
---    twoDrains lock
-    oneDrain lock
+    ctr1 <- atomically $ newTVar 5
+    ctr2 <- atomically $ newTVar 10
+    oneDrain lock ctr1 ctr2
+    -- twoDrains lock ctr1 ctr2
     milliSleep 60
 
-
-atomRead = atomically . readTVar
-dispVar x = atomRead x >>= print
 milliSleep = threadDelay . (*) 1000
