eef020b6a1c2402f5bbb66ec15c536efe3bb84f3
Emily Estes <emily.scott.estes@gmail.com>
2016-07-26 19:15:51 -0400

    Added a version of the Haskell STM example that removes all the syntactic sugar to reveal that it's just function calls. Added a slide about desugared Haskell. Minor fixes to the slides.

---
+++ threads_desugared.hs
@@ -0,0 +1,61 @@
+import Control.Concurrent
+import Control.Concurrent.STM
+import Control.Monad
+
+moveOne from to source sink =
+    (case ((==) 0 from) of
+        True -> (return ())
+        False ->
+            ((>>=)
+                (writeTVar source (from - 1))
+                (\_ -> (writeTVar sink (to + 1)))))
+
+drainOne source sink =
+    ((>>=)
+        (readTVar source)
+        (\from -> ((>>=)
+            (readTVar sink)
+            (\to -> (moveOne from to source sink)))))
+
+drain name source sink lock =
+    ((>>=)
+        (atomically (readTVar source))
+        (\from ->
+            ((>>=)
+                (atomically (readTVar sink))
+                (\to ->
+                    ((>>=)
+                        (withMVar lock (\_ -> putStrLn (((++) "draining " ((++) name ((++) " " ((++) (show from) ((++) ", " (show to)))))))))
+                        (\_ -> (case ((>) from 0) of
+                            True ->
+                                ((>>=)
+                                    (atomically (drainOne source sink))
+                                    (\_ -> ((>>=)
+                                        (milliSleep 1)
+                                        (\_ -> (drain name source sink lock)))))
+                            False -> (return ()))))))))
+
+
+oneDrain lock ctr1 ctr2 = forkIO (drain "a" ctr1 ctr2 lock)
+
+twoDrains lock ctr1 ctr2 =
+    ((>>=)
+        (forkIO (drain "a" ctr1 ctr2 lock))
+        (\_ -> (forkIO (drain "b" ctr2 ctr1 lock))))
+
+main =
+    ((>>=)
+        (newMVar ())
+        (\lock ->
+            ((>>=)
+                (atomically (newTVar 5))
+                (\ctr1 ->
+                    ((>>=)
+                        (atomically (newTVar 10)) 
+                        (\ctr2 ->
+                            ((>>=)
+                                (oneDrain lock ctr1 ctr2)
+                                --(twoDrains lock ctr1 ctr2)
+                                (\_ -> (milliSleep 60)))))))))
+
+milliSleep = threadDelay . ((*) 1000)
